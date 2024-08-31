6ab68fe440ad6f960fa9021b3a3036f73810ce02
sai kiran puppala <95897517+puppalasaikiran7@users.noreply.github.com>
2024-08-31 21:44:32 +0530

    Add files via upload

---
+++ string_basics.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    //strings basics 
+    string s="Python";
+
+    //accessing the character from the string 
+    cout<<s[2]<<endl;
+
+    //finding the length of the string  
+    cout<<s.size()<<endl;
+    
+}
